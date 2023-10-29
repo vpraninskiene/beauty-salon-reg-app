39ab84d5cbbde4db9375a1bccaf1c37d0487931c
Valentina P <valentina.praninskiene@gmail.com>
2023-10-29 10:01:00 +0200

    created footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "../Footer/Footer.css"
+
+export const Footer = () => {
+    return (
+        <div className="footer-wrapper">
+            <span>© All rights reserved</span>
+        </div>
+    )
+}
