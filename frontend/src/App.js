36278ba10805ea73769b08fbd418811a1d0b43f6
Valentina P <valentina.praninskiene@gmail.com>
2023-10-29 09:46:08 +0200

    created route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import { Header } from './components/Header/Header';
+import { LoginPage } from './pages/LoginPage/LoginPage';
+import { RegistrationPage } from './pages/RegistrationPage/RegistrationPage';
 
 function App() {
   return (
     <div className="App">
-    
+      <Header/>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegistrationPage/>}/>
+      </Routes>
     </div>
   );
 }
